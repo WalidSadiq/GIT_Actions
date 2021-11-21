1952df8f2ab781c4de7e2e24eaa3c7727f4df687
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 13:45:40 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 echo "hello!"
 ExecuteFiles=$(cat env_Instruction.json | jq '.[].ExecuteFiles')
-echo "${#ExecuteFiles[@]}"
-echo "${ExecuteFiles[@]}"
-for file in "${ExecuteFiles[@]}"; do
-echo "in Loop"
-echo "file = $file"
-echo "${ExecuteFiles[$file]}"
-echo "out Loop"
+#echo "${#ExecuteFiles[@]}"
+#echo "${ExecuteFiles[@]}"
+for ((i=0; i<=${#ExecuteFiles[@]}; i++)); do
+   echo "$i"
+   echo "in Loop"
+   #echo "file = $file"
+   echo "${ExecuteFiles[$i]}"
+   echo "out Loop"
 done
+#for i in "${#ExecuteFiles[@]}"; do
+#done
 Fallback_Scenario=$(cat env_Instruction.json | jq '.[].ExecuteFiles[].fallback')
-echo "${Fallback_Scenario[@]}"
+#echo "${Fallback_Scenario[@]}"
 Status=$(cat env_Instruction.json | jq '.[].status')
-echo "$Status"
+#echo "$Status"
