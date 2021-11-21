3562926fa5b57b9eb0b1f258f0ffe315c4bafc20
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 18:36:55 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-echo "Status = $(cat env_Instruction.json | jq -r '.[].status')"
+ENV="dev"
+echo "Status = $(cat $ENV_Env_Instruction.json | jq -r '.[].status')"
 if [[ "$(cat env_Instruction.json | jq -r '.[].status')" == "EXECUTE" ]]
 then
    for row in $(cat env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64'); do
@@ -10,9 +11,8 @@ then
      echo $(_jq '.filePath')
      filePathArray=($(echo $(_jq '.filePath') | tr "/" " "))
      echo "${#filePathArray[@]}"
-     echo "mvn apigee-config: ${filePathArray[${#filePathArray[@]}-1]} -P$ENV -Dusername=$machine_apigeeUsername -Dpassword=$machine_apigeePassword -Dorg=$ORG -Dapigee.config.options=$(_jq '.action')"
+     echo "mvn apigee-config:${filePathArray[${#filePathArray[@]}-1]} -P$ENV -Dusername=$machine_apigeeUsername -Dpassword=$machine_apigeePassword -Dorg=$ORG -Dapigee.config.options=$(_jq '.action')"
    done
 else
-   echo "FileStatusss"
    echo "Status = $(cat env_Instruction.json | jq -r '.[].status')"
 fi
