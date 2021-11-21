24cb8615091e5719aaba1234eac897156c481cc4
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 17:24:21 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -8,7 +8,7 @@ then
       echo ${row} | base64 --decode | jq -r ${1}
      }
      echo $(_jq '.filePath')
-     filePathArray=("echo $(_jq '.filePath') | tr '/' ' '")
+     filePathArray=(`echo $(_jq '.filePath') | tr '/' ' '`)
      echo "${#filePathArray[@]}"
      mvn_cmd = "mvn apigee-config: ${filePathArray[${#filePathArray[@]}]"
      echo ${mvn_cmd} 
