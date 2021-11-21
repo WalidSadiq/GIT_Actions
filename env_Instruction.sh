43acde94f0117ee75997649546e85ed4c55c4238
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 13:08:54 +0500

    Update env_Instruction.sh

--- env_Instruction.sh
+++ env_Instruction.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "hello!"
 ExecuteFiles=$(cat env_Instruction.json | jq '.[].ExecuteFiles[]')
+echo "${#ExecuteFiles[@]}"
 echo "${ExecuteFiles[@]}"
 Fallback_Scenario=$(cat env_Instruction.json | jq '.[].ExecuteFiles[].fallback')
 echo "${Fallback_Scenario[@]}"
