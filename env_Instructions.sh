a1ed91b5827153389c32a5fc341ca76216ea8ea8
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 18:59:06 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -11,6 +11,11 @@ then
      filePathArray=($(echo $(_jq '.filePath') | tr "/" " "))
      echo "${#filePathArray[@]}"
      echo "mvn apigee-config:${filePathArray[${#filePathArray[@]}-1]} -P$ENV -Dusername=$machine_apigeeUsername -Dpassword=$machine_apigeePassword -Dorg=$ORG -Dapigee.config.options=$(_jq '.action')"
+      if[[$?=1]]
+      then
+         echo "cmd failed"
+      else
+         echo "cmd success"
    done
 else
    echo "Status = $(cat Env_Instruction.json | jq -r '.[].status')"
