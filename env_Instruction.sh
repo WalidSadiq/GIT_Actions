db4772ceaa2b42f5edc52380cc36686d01054a2c
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 13:05:31 +0500

    Update env_Instruction.sh

--- env_Instruction.sh
+++ env_Instruction.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-
 echo "hello!"
 ExecuteFiles=$(cat env_Instruction.json | jq '.[].ExecuteFiles[]')
 echo "${ExecuteFiles[@]}"
-
 Fallback_Scenario=$(cat env_Instruction.json | jq '.[].ExecuteFiles[].fallback')
 echo "${Fallback_Scenario[@]}"
-
-Status=.[].status
+Status=$(cat env_Instruction.json | jq '.[].status')
 echo "$Status"
