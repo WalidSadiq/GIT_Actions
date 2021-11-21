11b79abf57a644a0e77526887a91510f7ec6b439
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 19:39:36 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -14,7 +14,8 @@ then
      echo "$?"
      if [ "$?" -ne 0 ]
      then
-        echo "Mvn Cmd failed"
+     echo "Fallback = $(cat Env_Instruction.json | jq -r '.[].$(_jq '.fallback')[] | @base64')"
+        echo "mvn apigee-config:${filePathArray[${#filePathArray[@]}-1]} -P$ENV -Dusername=$machine_apigeeUsername -Dpassword=$machine_apigeePassword -Dorg=$ORG -Dapigee.config.options=$(_jq '.action')"
      else
         echo "Mvn Cmd successfully executed"
      fi
