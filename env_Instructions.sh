4cb4ebcc194c1b4332c82c7e4da32fb1ffd3e79e
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 14:06:13 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 echo "hello!"
-#ExecuteFiles=$(cat env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64')
-#echo "${#ExecuteFiles[@]}"
-#echo "${ExecuteFiles[@]}"
+if [[ $(cat env_Instruction.json | jq '.[].status') = "EXECUTE" ]]
+then
 for row in $(cat env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64'); do
    echo "in Loop"
    echo ${row}
    echo ${row} | base64 --decode | jq -r ${1}
-   #echo "file = $file"
-   #echo "${ExecuteFiles[$row]}"
    echo "out Loop"
 done
-#for i in "${#ExecuteFiles[@]}"; do
-#done
-Fallback_Scenario=$(cat env_Instruction.json | jq '.[].ExecuteFiles[].fallback')
+else
+  echo "File Status = $(cat env_Instruction.json | jq '.[].status')" 
+fi
+#Fallback_Scenario=$(cat env_Instruction.json | jq '.[].ExecuteFiles[].fallback')
 #echo "${Fallback_Scenario[@]}"
-Status=$(cat env_Instruction.json | jq '.[].status')
+#Status=$(cat env_Instruction.json | jq '.[].status')
 #echo "$Status"
