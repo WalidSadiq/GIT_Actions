ebccd648f6c3dc52b949e7f26c5cab8003140c76
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 15:12:30 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-echo "hello!"
 echo "Status = $(cat env_Instruction.json | jq -r '.[].status')"
 if [[ "$(cat env_Instruction.json | jq -r '.[].status')" == "EXECUTE" ]]
 then
    for row in $(cat env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64'); do
       echo "in Loop"
       echo ${row}
-      echo ${row} | base64 --decode
-      #echo ${row} | base64 --decode | jq -r ${2}
+      #echo ${row} | base64 --decode
+      echo ${row} | base64 --decode | jq -r ${1}
       echo "out Loop"
    done
 else
