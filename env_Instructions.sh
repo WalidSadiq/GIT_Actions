d37afad7bcf4227a5fa4022bb782f1b274037025
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 16:40:30 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -5,7 +5,7 @@ then
    for row in $(cat env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64'); do
       echo "in Loop"
      _jq() {
-      echo ${row} | base64 --decode | jq -r ${1}
+      echo ${row} | base64 --decode
      }
      echo $(_jq '.filePath')
       echo ${row}'.filePath'| base64 --decode
