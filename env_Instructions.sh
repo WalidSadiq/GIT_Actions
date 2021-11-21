15bd5d5598a06bc95a53fb35e34a848100ab0dd0
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 14:57:58 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -3,7 +3,7 @@ echo "hello!"
 echo "Status = $(cat env_Instruction.json | jq -r '.[].status')"
 if [[ "$(cat env_Instruction.json | jq -r '.[].status')" == "EXECUTE" ]]
 then
-   for row in $(cat env_Instruction.json | jq '.[].ExecuteFiles[] | @base64'); do
+   for row in $(cat env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64'); do
       echo "in Loop"
       echo ${row}
       echo ${row} | base64 --decode | jq -r ${1}
