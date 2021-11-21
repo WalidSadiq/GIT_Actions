575c84228c46f9cea64894446a31c014f2272fb2
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-21 18:46:31 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -3,7 +3,7 @@ ENV="dev"
 echo "Status = $(cat ${ENV}_Env_Instruction.json | jq -r '.[].status')"
 if [[ "$(cat env_Instruction.json | jq -r '.[].status')" == "EXECUTE" ]]
 then
-   for row in $(cat env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64'); do
+   for row in $(cat ${ENV}_Env_Instruction.json | jq -r '.[].ExecuteFiles[] | @base64'); do
       echo "in Loop"
      _jq() {
       echo ${row} | base64 --decode | jq -r ${1}
@@ -14,5 +14,5 @@ then
      echo "mvn apigee-config:${filePathArray[${#filePathArray[@]}-1]} -P$ENV -Dusername=$machine_apigeeUsername -Dpassword=$machine_apigeePassword -Dorg=$ORG -Dapigee.config.options=$(_jq '.action')"
    done
 else
-   echo "Status = $(cat env_Instruction.json | jq -r '.[].status')"
+   echo "Status = $(cat ${ENV}_Env_Instruction.json | jq -r '.[].status')"
 fi
