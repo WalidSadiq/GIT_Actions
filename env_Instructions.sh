74b2959922883f225a0f0ab9a0c0093d88435156
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-22 12:22:02 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -18,10 +18,14 @@ then
         echo $(_jq '.fallback')
         echo "Fallback script started executing"
         #Fallbacks = "$(cat Env_Instruction.json | jq -r '.[].\${!(_jq '.fallback')}')"
-        eval "echo \$(cat Env_Instruction.json | jq -r '.[].$(_jq '.fallback')')"
+        eval "value=\$(cat Env_Instruction.json | jq -r '.[].$(_jq '.fallback')')"
+        echo "value = ${value}"
         #eval "value=\$(cat Env_Instruction.json | jq -r '.[].\${!(_jq '.fallback')}')"
         #eval ${Fallbacks}
-        echo "Fallback = $(cat Env_Instruction.json | jq -r '.[].$(_jq '.fallback')')"
+        #echo \$(cat Env_Instruction.json | jq -r '.[].${!(_jq '.fallback')}')
+        Fallback = "\$(cat Env_Instruction.json | jq -r '.[].$(_jq '.fallback')')"
+        echo ${Fallback}
+        
         for row in $(cat Env_Instruction.json | jq -r '.[]."${fallback}" | @base64'); do
             echo "in fallback Loop"
             _jq() {
