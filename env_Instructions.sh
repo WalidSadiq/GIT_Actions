c3f7c41d1bc348141e2a2066e73f2f61335cf723
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-11-22 12:13:38 +0500

    Update env_Instructions.sh

--- env_Instructions.sh
+++ env_Instructions.sh
@@ -17,7 +17,9 @@ then
        #fallback=$(_jq '.fallback')
         echo $(_jq '.fallback')
         echo "Fallback script started executing"
-        Fallbacks = "$(cat Env_Instruction.json | jq -r '.[].(${!(_jq '.fallback')})')"
+        #Fallbacks = "$(cat Env_Instruction.json | jq -r '.[].\${!(_jq '.fallback')}')"
+        eval "echo \$(cat Env_Instruction.json | jq -r '.[].{$(_jq '.fallback')}')"
+        #eval "value=\$(cat Env_Instruction.json | jq -r '.[].\${!(_jq '.fallback')}')"
         eval ${Fallbacks}
         echo "Fallback = $(cat Env_Instruction.json | jq -r '.[].$fallback | @base64')"
         for row in $(cat Env_Instruction.json | jq -r '.[]."${fallback}" | @base64'); do
