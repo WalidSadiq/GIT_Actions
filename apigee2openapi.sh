e02f086f8c0e17fb617dbed9866a16d03587ebe2
WalidSadiq <64598417+WalidSadiq@users.noreply.github.com>
2021-12-02 16:38:37 +0500

    Update apigee2openapi.sh

--- apigee2openapi.sh
+++ apigee2openapi.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "in apigee2openapi.sh"
+sudo npm install
 npm install -g apigee2openapi
 apigee2openapi -d ~/ -l ~/Drupal-Dummy_rev1_2021_12_02.zip -n Drupal-Dummy -e http://hiraaroob-fe1e7940-eval-test.apigee.net
